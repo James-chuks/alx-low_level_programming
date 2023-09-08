af210f43f624d694bea2e32229a459ddd899d1da
James-Ude <udejames27@gmail.com>
2023-09-08 04:39:21 -0700

    Alx Low Programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,17 +6,19 @@
  * Description: print the letters of the alphabet
  *
  * Return: Always 0 (Success)
- */
+ *
+ * /
 
 int main(void)
 {
-  int i = 97;
+	int i = 97;
+	while (i < 123)
+		{
+	
+	putchar(i);
+	i++;
+	}
 
-  while (i < 123)
-    {
-      putchar(i);
-      i++;
-    }
   putchar(10);
 
   return (0);
