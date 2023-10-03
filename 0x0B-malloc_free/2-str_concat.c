8a8bde19cc7512f2780eb3a032c860b8ff00165b
James-Ude <udejames27@gmail.com>
2023-10-03 10:08:58 -0700

    My ALX correction on strin_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,40 +1,29 @@
-#include "main.h"
 #include <stdlib.h>
 /**
  * str_concat - concatenates two strings
- * @s1: input one
- * @s2: input two
- *
- * Return: concate of s1 and s2
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to a new buffer containing s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, j;
+char *p;
+int s1count, s2count, sizeBuffer, i;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = j = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	conct = malloc(sizeof(char) * (i + j + 1));
-	if (conct == NULL)
-		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[i] != '\0')
-	{
-		conct[i] = s2[j];
-		i++, j++;
-	}
-	conct[i] = '\0';
-	return (conct);
+/*Check for valid strings*/
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (s1count = 0; s1[s1count]; s1count++)
+;
+for (s2count = 0; s2[s2count]; s2count++)
+;
+sizeBuffer = s1count + s2count + 1;
+p = malloc(sizeBuffer *sizeof(char));
+if (p == NULL)
+return (NULL);
+for (i = 0; i < sizeBuffer; i++)
+i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+return (p);
 }
