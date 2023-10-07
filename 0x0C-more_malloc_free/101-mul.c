cc6ecb28d7933cb38b3d622ab028258546c78903
James-Ude <udejames27@gmail.com>
2023-10-07 08:42:28 -0700

    commit new

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,134 +1,95 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#define ERR_MSG "Error"
 /**
- * _memset - fills memory with a constant byte
+ * is_digit - checks for non-digit elements in a string
+ * @s: the string
  *
- * @s: input pointer that represents memory block
- *     to fill
- * @b: characters to fill/set
- * @n: number of bytes to be filled
- *
- * Return: pointer to the filled memory area
+ * Return: 0 if non-digit, else 1
  */
-
-char *_memset(char *s, char b, unsigned int n)
+int is_digit(char *s)
 {
-  unsigned int i = 0;
+	int i = 0;
 
-  while (i < n)
-    {
-      s[i] = b;
-      i++;
-    }
-  return (s);
-}
-
-/**
- * _calloc - function that allocates memory
- *           for an array using memset
- *
- * @nmemb: size of array
- * @size: size of each element
- *
- * Return: pointer to new allocated memory
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-  char *ptr;
-
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  _memset(ptr, 0, nmemb * size);
-
-  return (ptr);
-}
-
-
-/**
- * multiply - initialize array with 0 byte
- *
- * @s1: string 1
- * @s2: string 2
- *
- * Return: nothing
- */
-
-void multiply(char *s1, char *s2)
-{
-  int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
-  char *ptr;
-  void *temp;
-
-  l1 = _length(s1);
-  l2 = _length(s2);
-  tmp = l2;
-  total_l = l1 + l2;
-  ptr = _calloc(sizeof(int), total_l);
-
-  /* store our pointer address to free later */
-  temp = ptr;
-
-  for (l1--; l1 >= 0; l1--)
-    {
-      f_digit = s1[l1] - '0';
-      res = 0;
-      l2 = tmp;
-      for (l2--; l2 >= 0; l2--)
+	while (s[i])
 	{
-	  s_digit = s2[l2] - '0';
-	  res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
-	  ptr[l1 + l2 + 1] = res % 10;
-	  res /= 10;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
-      if (res)
-	ptr[l1 + l2 + 1] = res % 10;
-    }
-
-  while (*ptr == 0)
-    {
-      ptr++;
-      total_l--;
-    }
-
-  for (i = 0; i < total_l; i++)
-    printf("%i", ptr[i]);
-  printf("\n");
-  free(temp);
+	return (1);
 }
-
-
 /**
- * main - Entry point
+ * _strlen - returns the length of the string
+ * @s: the string
  *
- * Description: a program that multiplies
- *            two positive numbers
- *
- * @argc: number of arguments
- * @argv: arguments array
- *
- * Return: 0 on success 98 on faliure
+ * Return: the length of the string
  */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i] != '\0')
+{
+	i++;
+}
+return (i);
+}
+/**
+ * errors - handles errors for main function
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-  char *n1 = argv[1];
-  char *n2 = argv[2];
+	char *s1, *s2;
+	int len1, len2, len, i, re, d1, d2, *o, a = 0;
 
-  if (argc != 3 || check_number(n1) || check_number(n2))
-    error_exit();
-
-  if (*n1 == '0' || *n2 == '0')
-    {
-      _putchar('0');
-      _putchar('\n');
-    }
-  else
-    multiply(n1, n2);
-  return (0);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	o = malloc(sizeof(int) * len);
+	if (!o)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		o[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len--)
+	{
+		d1 = s1[len1] - '0';
+		re = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			d2 = s2[len2] - '0';
+			re += o[len1 + len2 + 1] + (d1 * d2);
+			o[len1 + len2 + 1] = re % 10;
+			re /= 10;
+		}
+		if (re > 0)
+			o[len1 + len2 + 1] += re;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (o[i])
+			a = 1;
+		if (a)
+			_putchar(o[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(o);
+	return (0);
 }
